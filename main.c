93e71216116ba9e1a6e77e7bd09e7d74fcafb31e
Abril Nataly Calatayud <acalatayud@fie.undef.edu.ar>
2023-03-31 10:38:58 -0300

    second commit

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int nota, suma=0, cont=0, prom;
+    while (notas!=1){
+        printf("Ingrese las notas de los alumnos del curso:\n");
+        scanf("%d", &nota);
+        suma=suma+nota;
+        cont++;
+    }
+    prom=suma/cont;
+    printf("El promedio es: %d", prom);
     return 0;
 }
